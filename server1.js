a8ba915e3a45139b4af8d68b1117f4ebc95ebe78
Bram van de Griend <shep.ukn@gmail.com>
2017-04-20 11:42:17 +0200

    Het woord 'de' toegevoegd.

--- server1.js
+++ server1.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response)
 	response.writeHead(200,{'Content-Type': 'text/json'});
 	var json = JSON.stringify(
 	{
-		tekst: "Dit is JSON!"
+		tekst: "Dit is de JSON!"
 	});
 	response.end(json);
 }).listen(3000);
