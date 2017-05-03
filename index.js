18cf4caf0922d6d4a4fbfd04f9963df2e44e14f5
Bram van de Griend <shep.ukn@gmail.com>
2017-05-03 14:25:45 +0200

    Updated index.js code.

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 
+var exampleObject = {
+	greeting: "Hello world!",
+	farewell: "Goodbye world..."
+}
+
 app.get('/', function(request, response) {
- response.send('Hello Avans, GET request received!');
+	response.contentType('application/json');
+	response.json(exampleObject);
 })
+
 app.get('/about', function(request, response) {
- response.send('Written by Bram');
+	response.send('Written by Bram.');
 })
+
 app.post('/', function(request, response) {
- response.send('Hello Avans, POST request received!');
+	response.send('Hello Avans, POST request received!');
 })
+
 app.put('/', function(request, response) {
- response.send('Hello Avans, PUT request received!');
+	response.send('Hello Avans, PUT request received!');
 })
+
 app.all('*', function(request, response) {
- response.status(404);
- response.send('404 - Not found');
+	response.status(404);
+	response.send('404 - Not found');
 })
 
 
 
-
+// Starter message.
 app.listen(3000, function() {
- console.log('Server app is listening on port 3000');
+	console.log('Server app is listening on port 3000');
 })
